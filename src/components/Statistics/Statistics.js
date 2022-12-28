58bb0423c36d73792d9310b474a69f26d5caf3fa
AsifKhan-27 <asifak001@gmail.com>
2022-12-29 02:02:54 +0600

    added chart

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { useLoaderData } from 'react-router-dom';
 
 const Statistics = () => {
+    const quizTopics=useLoaderData();
+    const topics=quizTopics["data"]; 
+    const data=[];
+    topics.map(topic=>data.push(topic.total));
+    // for (let topic in topics){
+        // data.push(topic.total);
+    // }
+    console.log(data);
     return (
         <div>
-            <h2>Statistics</h2>
+            <h2 className='mb-5'>Statistics</h2>
+            <LineChart className='mx-auto mt-4' width={500} height={300} data={topics}>
+                <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+                <XAxis dataKey="name" />
+                <YAxis />
+            </LineChart>
         </div>
     );
 };
